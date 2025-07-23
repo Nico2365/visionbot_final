6689ad5bcc9a7513f70b02771405dfba5a25b722
Nico Gruendler <nico.gruendler@gmx.de>
2025-07-23 14:36:11 +0200

    Initial clean commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>VisionBot Dashboard</h1>
+      <p>Bereit für den nächsten Trade!</p>
+    </div>
+  );
+}
+
+export default App;
